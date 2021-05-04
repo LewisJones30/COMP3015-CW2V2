2deed81f0191b65dfa81d8a6e94bb49dac6e4308
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2021-05-04 16:58:14 +0100

    Shadows with raptors working correctly.

--- scenebasic_uniform.cpp
+++ scenebasic_uniform.cpp
@@ -15,190 +15,228 @@ using std::endl;
 #include "helper/glslprogram.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/constants.hpp>
 #include "helper/torus.h"
 #include "helper/texture.h"
+#include <random>
 
 
 using glm::vec3;
 using glm::mat4;
 using glm::vec4;
 using glm::mat3;
-SceneBasic_Uniform::SceneBasic_Uniform() : plane(30.0f, 30.0f, 100, 100), angle(0.0f), tPrev(0.0f), rotSpeed(glm::pi<float>() / 8.0f), sky(100.0f)
+SceneBasic_Uniform::SceneBasic_Uniform() : rotSpeed(0.1f), tPrev(0), plane(10.0f, 10.0f, 2, 2, 5.0f, 5.0f)
 {
-    mesh = ObjMesh::load("media/Trex.obj", true);
-    mesh1 = ObjMesh::load("media/Raptor.obj", true);
-    mesh2 = ObjMesh::load("media/Raptor.obj", true);
+    spot = ObjMesh::loadWithAdjacency("media/Raptor.obj");
 }
 
 void SceneBasic_Uniform::initScene()
 {
     compile();
+
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glClearStencil(0);
     glEnable(GL_DEPTH_TEST);
 
-    view = glm::lookAt(vec3(-20.0f, 10.5f, -20.0f), vec3(0.0f, 0.0f, 0.0f),
-        vec3(0.0f, 1.0f, 0.0f));
-    projection = mat4(1.0f);
-    angle = glm::radians(90.0f);
-    float x, z;
-    prog.setUniform("lights[0].Position", vec3(10.0f, 10.0f, 10.0f));
-    prog.setUniform("lights[0].L", vec3(0.0f, 0.8f, 0.0f)); //Set the light colour
-    prog.setUniform("lights[0].La", vec3(0.0f, 0.05f, 0.0f)); //Set the ambient light colour
-    //Setup first spotlight
-    prog.setUniform("Spot[0].L", vec3(2.0f));
-    prog.setUniform("Spot[0].La", vec3(0.0f));
-    prog.setUniform("Spot[0].Exponent", 20.0f);
-    prog.setUniform("Spot[0].Cutoff", glm::radians(3.14f * 4.0f));
-    //Setup second spotlight on the Raptor
-    prog.setUniform("Spot[1].L", vec3(2.0f, 0.2f, 0.2f));
-    prog.setUniform("Spot[1].La", vec3(0.0f));
-    prog.setUniform("Spot[1].Exponent", 20.0f);
-    prog.setUniform("Spot[1].Cutoff", glm::radians(3.14f * 4.0f));
-    //Bind the texture to the spotlight
-    GLuint texID = Texture::loadTexture("media/texture/Raptor_low_Corpo_Roughness.png");
-    GLuint cubeTex =
-        Texture::loadCubeMap("media/texture/cube/pisa/forest", ".png");
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texID);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, cubeTex);
+    angle = 0.0f;
+    setupFBO();
 
+    renderProg.use();
+    renderProg.setUniform("LightIntensity", vec3(1.0f));
+
+    GLfloat verts[] = {
+        -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 1.0f, 1.0f, 0.0f, -1.0f, 1.0f, 0.0f
+    };
+    GLuint bufHandle;
+    glGenBuffers(1, &bufHandle);
+    glBindBuffer(GL_ARRAY_BUFFER, bufHandle);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 3 * sizeof(GLfloat), verts, GL_STATIC_DRAW);
+
+    glGenVertexArrays(1, &fsQuad);
+    glBindVertexArray(fsQuad);
+
+    glBindBuffer(GL_ARRAY_BUFFER, bufHandle);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    glActiveTexture(GL_TEXTURE2);
+    spotTex = Texture::loadTexture("media/texture/Raptor_low_Corpo_BaseColor.png");
+    brickTex = Texture::loadTexture("media/texture/brick1.jpg");
+
+    updateLight();
+    
+    renderProg.use();
+    renderProg.setUniform("Tex", 2);
+
+    compProg.use();
+    compProg.setUniform("DiffSpecTex", 0);
+
+    this->animate(true);
+
+    
 
 }
+void SceneBasic_Uniform::updateLight()
+{
+    lightPos = vec4(5.0f * vec3(cosf(angle) * 5.0f, 1.5f, sinf(angle) * 5.0f), 1.0f);
+}
+
 void SceneBasic_Uniform::compile()
 {
-	try {
-		prog.compileShader("shader/Blinn-Phong_Vertex_Shader.vert");
-		prog.compileShader("shader/Blinn-Phong_Fragment.frag");
-		prog.link();
-		prog.use();
-	} catch (GLSLProgramException &e) {
-		cerr << e.what() << endl;
-		exit(EXIT_FAILURE);
-	}
+    try
+    {
+        volumeProg.compileShader("shader/Blinn-Phong_Fragment.frag");
+        volumeProg.compileShader("shader/Blinn-Phong_Vertex_Shader.vert");
+        volumeProg.compileShader("shader/shadows.geom");
+        volumeProg.link();
+
+        renderProg.compileShader("shader/shadowvolume-render.frag");
+        renderProg.compileShader("shader/shadowvolume-render.vs");
+        renderProg.link();
+
+        compProg.compileShader("shader/solid.vs");
+        compProg.compileShader("shader/solid.frag");
+        compProg.link();
+    }
+    catch (GLSLProgramException& e)
+    {
+        cerr << e.what() << endl;
+        exit(EXIT_FAILURE);
+    }
 }
 
 void SceneBasic_Uniform::update(float t)
 {
     float deltaT = t - tPrev;
     if (tPrev == 0.0f)
+    {
         deltaT = 0.0f;
+    }
     tPrev = t;
-    angle += rotSpeed * deltaT;
-    if (angle > glm::two_pi<float>())
-        angle -= glm::two_pi<float>();
-
-
+    if (animating())
+    {
+        angle += deltaT * rotSpeed;
+        if (angle > glm::two_pi<float>())
+        {
+            angle -= glm::two_pi<float>();
+        }
+        updateLight();
+    }
    
 }
 
 void SceneBasic_Uniform::render()
 {
-glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-vec4 lightPos = vec4(1.0f, 28.5f, 0.0f, 1.0f);
-prog.setUniform("Spot[0].Position", vec3(view * lightPos));
-mat3 normalMatrix = mat3(vec3(view[0]), vec3(view[1]), vec3(view[2]));
-prog.setUniform("Spot[0].Direction", normalMatrix * vec3(-lightPos));
-//Set the second spotlight's uniforms
-lightPos = vec4(-4.0f, 7.5f, 3.0f, 1.0f); //As there is no view matrix applied - this will be based on the camera.
-prog.setUniform("Spot[1].Position", vec3(lightPos));
-prog.setUniform("Spot[1].Direction", normalMatrix * vec3(-lightPos));
-
-//Set material uniforms for the first mesh, apply transformations and render. (T-Rex)
- prog.setUniform("Material.Kd", 0.4f, 0.4f, 0.4f);
- prog.setUniform("Material.Ks", 0.9f, 0.9f, 0.9f);
- prog.setUniform("Material.Ka", 0.5f, 0.5f, 0.5f);
- prog.setUniform("Material.Shininess", 180.0f);
- prog.setUniform("test1", vec3(3.0f));
- model = mat4(1.0f);
- model = glm::rotate(model, glm::radians(3.14f * 0.5f), vec3(0.0f, 1.0f, 0.0f));
- model = glm::scale(model, vec3(0.4f, 0.4f, 0.4f));
- model = glm::translate(model, vec3(-2.0f, 5.0f, -1.0f));
- setMatrices();
- mesh->render();
- //Set material uniforms for the second model, apply transformations and render. (Raptor #1)
- prog.setUniform("Material.Kd", 0.4f, 0.4f, 0.4f);
- prog.setUniform("Material.Ks", 0.9f, 0.9f, 0.9f);
- prog.setUniform("Material.Ka", 0.5f, 0.5f, 0.5f);
- prog.setUniform("Material.Shininess", 55.0f);
- prog.setUniform("test1", vec3(5.0f));
-
- model = mat4(1.0f);
- model = glm::rotate(model, glm::radians(3.14f * 0.5f), vec3(0.0f, 1.0f, 0.0f));
- model = glm::translate(model, vec3(-10.0f, 1.2f, 1.0f));
- model = glm::scale(model, vec3(0.5f, 0.5f, 0.5f));
- setMatrices();
- mesh1->render();
-
- //Set material uniforms for the third model, apply transformations and render. This also includes the fog.(Raptor #2)
- prog.setUniform("Material.Kd", 0.4f, 0.4f, 0.4f);
- prog.setUniform("Material.Ks", 0.9f, 0.9f, 0.9f);
- prog.setUniform("Material.Ka", 0.5f, 0.5f, 0.5f);
- prog.setUniform("Material.Shininess", 300.0f);
- prog.setUniform("Fog.MaxDist", 30.0f);
- prog.setUniform("Fog.MinDist", 0.1f);
- prog.setUniform("Fog.Color", vec3(0.2f, 0.2f, 0.2f));
- prog.setUniform("test1", vec3(0.0f));
- model = mat4(1.0f);
- model = glm::rotate(model, glm::radians(3.14f * 0.5f), vec3(0.0f, 1.0f, 0.0f));
- model = glm::translate(model, vec3(10.0f, 1.2f, 1.0f));
- model = glm::scale(model, vec3(0.5f, 0.5f, 0.5f));
- setMatrices();
- mesh2->render();
-
-
-
- //Render the plane.
- model = mat4(1.0f);
- prog.setUniform("test1", vec3(4.0f));
- setMatrices();
- plane.render();
-
-
- vec3 cameraPos = vec3(15.0f * cos(angle), 2.0f, 15.0f * sin(angle));
- view = glm::lookAt(cameraPos, vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f,
-     0.0f));
- // Draw sky
- prog.use();
- model = mat4(1.0f);
- prog.setUniform("test1", vec3(2.0f));
- setMatrices();
- sky.render();
+    pass1();
+    glFlush();
+    pass2();
+    glFlush();
+    pass3();
 }
 
-
-void SceneBasic_Uniform::setMatrices()
+void SceneBasic_Uniform::pass1()
 {
-    mat4 mv;
-    mv = view * model;
-    prog.setUniform("ModelViewMatrix", mv);
-    prog.setUniform("NormalMatrix", glm::mat3(vec3(mv[0]), vec3(mv[1]), vec3(mv[2])));
-    prog.setUniform("MVP", projection * mv);
+    glDepthMask(GL_TRUE);
+    glDisable(GL_STENCIL_TEST);
+    glEnable(GL_DEPTH_TEST);
+    projection = glm::infinitePerspective(glm::radians(50.0f), (float)width / height, 0.5f);
+    view = glm::lookAt(vec3(5.0f, 5.0f, 5.0f), vec3(0, 2, 0), vec3(0, 1, 0));
+
+    renderProg.use();
+    renderProg.setUniform("LightPosition", view * lightPos);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, colorDepthFBO);
+    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+    drawScene(renderProg, false);
+}
+
+void SceneBasic_Uniform::pass2()
+{
+    volumeProg.use();
+    volumeProg.setUniform("LightPosition", view * lightPos);
+
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, colorDepthFBO);
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+    glBlitFramebuffer(0, 0, width - 1, height - 1, 0, 0, width - 1, height - 1, GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
+    glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+    glDepthMask(GL_FALSE);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    glClear(GL_STENCIL_BUFFER_BIT);
+    glEnable(GL_STENCIL_TEST);
+    glStencilFunc(GL_ALWAYS, 0, 0xfffff);
+    glStencilOpSeparate(GL_FRONT, GL_KEEP, GL_KEEP, GL_INCR_WRAP);
+    glStencilOpSeparate(GL_BACK, GL_KEEP, GL_KEEP, GL_DECR_WRAP);
+    drawScene(volumeProg, true);
+
+    glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+
+}
+void SceneBasic_Uniform::pass3()
+{
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE, GL_ONE);
+
+    glStencilFunc(GL_EQUAL, 0, 0xffff);
+    glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+
+    compProg.use();
+
+    model = mat4(1.0f);
+    projection = model;
+    view = model;
+    setMatrices(compProg);
+
+    glBindVertexArray(fsQuad);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+    glBindVertexArray(0);
+
+    glDisable(GL_BLEND);
+    glEnable(GL_DEPTH_TEST);
+
 }
 
 void SceneBasic_Uniform::setupFBO()
 {
     GLuint depthBuf;
-    // Create and bind the FBO
-    glGenFramebuffers(1, &hdrFBO);
-    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
-    // The depth buffer
     glGenRenderbuffers(1, &depthBuf);
     glBindRenderbuffer(GL_RENDERBUFFER, depthBuf);
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
-    // The HDR color buffer
+
+    GLuint ambBuf;
+    glGenRenderbuffers(1, &ambBuf);
+    glBindRenderbuffer(GL_RENDERBUFFER, ambBuf);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA, width, height);
+
     glActiveTexture(GL_TEXTURE0);
-    glGenTextures(1, &hdrTex);
-    glBindTexture(GL_TEXTURE_2D, hdrTex);
-    glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGB32F, width, height);
+    GLuint diffSpecTex;
+    glGenTextures(1, &diffSpecTex);
+    glBindTexture(GL_TEXTURE_2D, diffSpecTex);
+    glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, width, height);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // Attach the images to the framebuffer
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
-        GL_RENDERBUFFER, depthBuf);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
-        hdrTex, 0);
-    GLenum drawBuffers[] = { GL_NONE, GL_COLOR_ATTACHMENT0 };
+
+    glGenFramebuffers(1, &colorDepthFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, colorDepthFBO);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBuf);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, ambBuf);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, diffSpecTex, 0);
+
+    GLenum drawBuffers[] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
     glDrawBuffers(2, drawBuffers);
+    GLenum result = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+    if (result == GL_FRAMEBUFFER_COMPLETE)
+    {
+        printf("Framebuffer is complete.\n");
+    }
+    else
+    {
+        printf("Framebuffer is not complete.\n");
+    }
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
@@ -207,43 +245,73 @@ void SceneBasic_Uniform::resize(int w, int h)
     glViewport(0, 0, w, h);
     width = w;
     height = h;
-    projection = glm::perspective(glm::radians(70.0f), (float)w / h, 0.3f, 100.0f);
 }
-void SceneBasic_Uniform::drawScene()
+
+void SceneBasic_Uniform::setMatrices(GLSLProgram& prog)
 {
-    vec3 intense = vec3(1.0f);
-    prog.setUniform("Lights[0].L", intense);
-    prog.setUniform("Lights[1].L", intense);
-    prog.setUniform("Lights[2].L", intense);
-    vec4 lightPos = vec4(0.0f, 4.0f, 2.5f, 1.0f);
-    lightPos.x = -7.0f;
-    prog.setUniform("Lights[0].Position", view * lightPos);
-    lightPos.x = 0.0f;
-    prog.setUniform("Lights[1].Position", view * lightPos);
-    lightPos.x = 7.0f;
-    prog.setUniform("Lights[2].Position", view * lightPos);
-    prog.setUniform("Material.Kd", 0.9f, 0.3f, 0.2f);
-    prog.setUniform("Material.Ks", 1.0f, 1.0f, 1.0f);
-    prog.setUniform("Material.Ka", 0.2f, 0.2f, 0.2f);
-    prog.setUniform("Material.Shininess", 100.0f);
-    // The backdrop plane
-    model = glm::rotate(mat4(1.0f), glm::radians(90.0f), vec3(1.0f, 0.0f,0.0f));
-    setMatrices();
-    plane.render();
-    // The bottom plane
-    model = glm::translate(mat4(1.0f), vec3(0.0f, -5.0f, 0.0f));
-    setMatrices();
-    plane.render();
-    // Top plane
-    model = glm::translate(mat4(1.0f), vec3(0.0f, 5.0f, 0.0f));
-    model = glm::rotate(model, glm::radians(180.0f), vec3(1.0f, 0.0f, 0.0f));
-    setMatrices();
-    plane.render();
-    prog.setUniform("Material.Kd", vec3(0.4f, 0.9f, 0.4f));
-    model = glm::translate(mat4(1.0f), vec3(-3.0f, -3.0f, 2.0f));
-    setMatrices();
-    prog.setUniform("Material.Kd", vec3(0.4f, 0.4f, 0.9f));
-    model = glm::translate(mat4(1.0f), vec3(3.0f, -5.0f, 1.5f));
-    model = glm::rotate(model, glm::radians(-90.0f), vec3(1.0f, 0.0f, 0.0f));
-    setMatrices();
+    mat4 mv = view * model;
+    prog.setUniform("ModelViewMatrix", mv);
+    prog.setUniform("ProjMatrix", projection);
+    prog.setUniform("NormalMatrix", glm::mat3(vec3(mv[0]), vec3(mv[1]), vec3(mv[2])));
+}
+
+void SceneBasic_Uniform::drawScene(GLSLProgram& prog, bool onlyShadowCasters)
+{
+    vec3 color;
+    if (!onlyShadowCasters)
+    {
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, spotTex);
+        color = vec3(1.0f);
+        prog.setUniform("Ka", color * 0.1f);
+        prog.setUniform("Kd", color);
+        prog.setUniform("Ks", vec3(0.9f));
+        prog.setUniform("Shininess", 150.0f);
+    }
+    model = mat4(1.0f);
+    model = glm::translate(model, vec3(-2.3f, 1.0f, 0.2f));
+    model = glm::rotate(model, glm::radians(180.0f), vec3(0.0f, 1.0f, 0.0f));
+    model = glm::scale(model, vec3(0.25f));
+    setMatrices(prog);
+    spot->render();
+    model = mat4(1.0f);
+    model = glm::translate(model, vec3(0.5f, 1.0f, 2.7f));
+    model = glm::rotate(model, glm::radians(180.0f), vec3(0.0f, 1.0f, 0.0f));
+    model = glm::scale(model, vec3(0.25f));
+    setMatrices(prog);
+    spot->render();
+
+    model = mat4(1.0f);
+    model = glm::translate(model, vec3(2.5f, 1.0f, -1.2f));
+    model = glm::rotate(model, glm::radians(180.0f), vec3(0.0f, 1.0f, 0.0f));
+    model = glm::scale(model, vec3(0.25f));
+    setMatrices(prog);
+    spot->render();
+
+    if (!onlyShadowCasters)
+    {
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, brickTex);
+        color = vec3(0.5f);
+        prog.setUniform("Kd", color);
+        prog.setUniform("Ks", vec3(0.0f));
+        prog.setUniform("Ka", vec3(0.1f));
+        prog.setUniform("Shininess", 1.0f);
+        model = mat4(1.0f);
+        setMatrices(prog);
+        plane.render();
+        model = mat4(1.0f);
+        model = glm::translate(model, vec3(-5.0f, 5.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(90.0f), vec3(1, 0, 0));
+        model = glm::rotate(model, glm::radians(-90.0f), vec3(0.0f, 0.0f, 1.0f));
+        setMatrices(prog);
+        plane.render();
+        model = mat4(1.0f);
+        model = glm::translate(model, vec3(0.0f, 5.0f, -5.0f));
+        model = glm::rotate(model, glm::radians(90.0f), vec3(1.0f, 0.0f, 0.0f));
+        setMatrices(prog);
+        plane.render();
+        model = mat4(1.0f);
+    }
+
 }
--- scenebasic_uniform.h
+++ scenebasic_uniform.h
@@ -13,27 +13,28 @@
 #include "helper/objmesh.h"
 #include "helper/sphere.h"
 #include "helper/skybox.h"
+#include "helper/frustum.h"
 
 class SceneBasic_Uniform : public Scene
 {
 private:
-    GLSLProgram prog;
+    GLSLProgram volumeProg, renderProg, compProg;
+    GLuint colorDepthFBO, fsQuad;
+    GLuint spotTex, brickTex;
+
     Plane plane;
-    std::unique_ptr<ObjMesh> mesh;
-    std::unique_ptr<ObjMesh> mesh1;
-    std::unique_ptr<ObjMesh> mesh2;
-    GLuint hdrFBO;
-    GLuint quad;
-    GLuint hdrTex, avgTex;
-    SkyBox sky;
+    std::unique_ptr<ObjMesh> spot;
+    glm::vec4 lightPos;
     float angle, tPrev, rotSpeed;
-    void setMatrices();
+
+    void setMatrices(GLSLProgram& prog);
     void compile();
     void setupFBO();
+    void drawScene(GLSLProgram& prog, bool onlyShadowCasters);
     void pass1();
     void pass2();
-    void computeLogAveLuminance();
-    void drawScene();
+    void pass3();
+    void updateLight();
 
 public:
     SceneBasic_Uniform();
