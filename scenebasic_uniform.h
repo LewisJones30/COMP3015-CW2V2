316a2666f1160e7e4aac1b6b5d56009a6554405a
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2021-05-05 10:41:09 +0100

    Added new additional files for creating particle streams.
    This is a backup before I delve into adding the code for particle streams.

--- scenebasic_uniform.h
+++ scenebasic_uniform.h
@@ -14,18 +14,33 @@
 #include "helper/sphere.h"
 #include "helper/skybox.h"
 #include "helper/frustum.h"
+#include "helper/random.h"
+#include "helper/grid.h"
 
 class SceneBasic_Uniform : public Scene
 {
 private:
-    GLSLProgram volumeProg, renderProg, compProg;
+    GLSLProgram volumeProg, renderProg, compProg, prog, flatProg;
     GLuint colorDepthFBO, fsQuad;
     GLuint spotTex, brickTex;
-
     Plane plane;
     std::unique_ptr<ObjMesh> spot;
     glm::vec4 lightPos;
     float angle, tPrev, rotSpeed;
+
+    Random rand;
+    glm::vec3 emitterPos, emitterDir;
+
+    GLuint posBuf[2], velBuf[2], age[2];
+    GLuint particleArray[2];
+    GLuint feedback[2];
+    GLuint drawBuf;
+    Grid grid;
+
+    int nParticles;
+    float particleLifetime;
+    float time, deltaT;
+
 
     void setMatrices(GLSLProgram& prog);
     void compile();
